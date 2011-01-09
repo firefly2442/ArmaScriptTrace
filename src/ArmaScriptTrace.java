9789dfaba6487b15e0a8757a3dc0028be9b78aa5
firefly2442 <firefly2442@hotmail.com>
2011-01-09 12:15:19 -0600

    rev version

--- src/ArmaScriptTrace.java
+++ src/ArmaScriptTrace.java
@@ -16,7 +16,7 @@ import javax.swing.filechooser.FileSystemView;
  
 public class ArmaScriptTrace extends JPanel implements ActionListener
 {
-	public static final String VERSION = "0.4";
+	public static final String VERSION = "0.5";
 	static GUI theGUI;
 	static Preferences prefs;
 	
